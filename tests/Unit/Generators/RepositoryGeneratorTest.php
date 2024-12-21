20280749fbdec3d8771ff06e6a4d4e999303ba34
zaidysf <zaidysf@users.noreply.github.com>
2024-12-21 20:27:24 +0000

    Fix styling

--- tests/Unit/Generators/RepositoryGeneratorTest.php
+++ tests/Unit/Generators/RepositoryGeneratorTest.php
@@ -12,7 +12,7 @@ class RepositoryGeneratorTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
-        $this->generator = new RepositoryGenerator();
+        $this->generator = new RepositoryGenerator;
     }
 
     public function test_can_generate_repository_files(): void
